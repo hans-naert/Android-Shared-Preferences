184c6b62c989a189bc94759ad6058d1f9087998b
Hans Naert <hans.naert@vives.be>
2023-03-17 10:17:07 +0100

    write data to shared preferences

--- app/src/main/java/com/example/sharedpreferencesapplication/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferencesapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.sharedpreferencesapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sharedpreferencesapplication.databinding.ActivityMainBinding
@@ -12,5 +13,12 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.saveButton.setOnClickListener {
+            val editor = getSharedPreferences("data", Context.MODE_PRIVATE).edit()
+            editor.putString("name", "Tom")
+            editor.putInt("age", 28)
+            editor.putBoolean("married", false)
+            editor.apply()
+        }
     }
 }
