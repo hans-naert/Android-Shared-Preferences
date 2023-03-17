a4c6d9e00961567dd33a292880d0596856819e4c
Hans Naert <hans.naert@vives.be>
2023-03-17 10:20:59 +0100

    restore data from shared preferences

--- app/src/main/java/com/example/sharedpreferencesapplication/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferencesapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.sharedpreferencesapplication
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.sharedpreferencesapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +20,16 @@ class MainActivity : AppCompatActivity() {
             editor.putInt("age", 28)
             editor.putBoolean("married", false)
             editor.apply()
+        }
+
+        binding.restoreButton.setOnClickListener {
+            val prefs = getSharedPreferences("data", Context.MODE_PRIVATE)
+            val name = prefs.getString("name", "")
+            val age = prefs.getInt("age", 0)
+            val married = prefs.getBoolean("married", false)
+            Log.d("MainActivity", "name is $name")
+            Log.d("MainActivity", "age is $age")
+            Log.d("MainActivity", "married is $married")
         }
     }
 }
