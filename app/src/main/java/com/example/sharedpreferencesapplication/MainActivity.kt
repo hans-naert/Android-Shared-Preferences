1050a78ea420818ddfc995b892632711f3b192b3
Hans Naert <hans.naert@vives.be>
2023-03-17 10:06:19 +0100

    add binding

--- app/src/main/java/com/example/sharedpreferencesapplication/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferencesapplication/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.sharedpreferencesapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sharedpreferencesapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
